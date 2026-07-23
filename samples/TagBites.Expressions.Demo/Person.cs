4d946f991631b1a8f60c4635240840adb384147e
Tomasz Malik <tomasz.malik@outlook.com>
2026-07-23 17:29:36 +0200

    demo: more samples, variables

---
+++ samples/TagBites.Expressions.Demo/Person.cs
@@ -0,0 +1,9 @@
+namespace TagBites.Expressions.Demo;
+
+public class Person
+{
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+
+    public override string ToString() => $"{Name} ({Age})";
+}
